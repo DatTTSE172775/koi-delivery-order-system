27aac0a5b8ebe2130da43bb6c0e32b4b6c55b963
DatTTSE172775 <datttse172775@fpt.edu.vn>
2024-10-10 11:47:47 +0700

    order table

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import FilterDelivery from "../components/filter-delivery/FilterDelivery";
+import OrderTable from "../components/order-table/OrderTable";
 import Overview from "../components/overview/Overview";
 
 const DashboardPage: React.FC = () => {
@@ -6,6 +8,8 @@ const DashboardPage: React.FC = () => {
     <div>
       <h1>Dashboard</h1>
       <Overview />
+      <FilterDelivery />
+      <OrderTable />
       {/* <SalesChart /> */}
     </div>
   );
